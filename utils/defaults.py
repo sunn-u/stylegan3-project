246086e0a822b58be302be62aabd37fcfb30cca1
sunn-u <tjsntjsn20@gmail.com>
2021-10-24 16:33:46 +0900

    [2021-10-24] add yaml/configs func

---
+++ utils/defaults.py
@@ -0,0 +1,18 @@
+
+import yaml
+import torch
+
+from data.types import Argparse
+
+
+def load_yaml(config_path: str) -> yaml:
+    with open(config_path, encoding='utf-8') as file:
+        configs = yaml.load(file, Loader=yaml.FullLoader)
+    return configs
+
+def setup_configs(args: Argparse, configs: dict) -> dict:
+    configs['SOLVER']['DEVICE'] = is_gpu(args)
+    return configs
+
+def is_gpu(args: Argparse):
+    return torch.device(f'cuda:{args.gpu_id}' if torch.cuda.is_available() else 'cpu')
