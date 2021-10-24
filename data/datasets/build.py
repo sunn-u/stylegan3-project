1ec2a55f4fd628377ae9e4e9aa918d27752fa32d
sunn-u <tjsntjsn20@gmail.com>
2021-10-24 21:45:04 +0900

    [2021-10-24] add datasets func

---
+++ data/datasets/build.py
@@ -0,0 +1,10 @@
+
+from fvcore.common.registry import Registry
+
+DATASETS_REGISTRY = Registry("DATASETS")
+DATASETS_REGISTRY.__doc__ = " Registry for Datasets "
+
+def build_datasets(configs: dict):
+    name = configs["DATA"]["NAME"]
+    loader = DATASETS_REGISTRY.get(name)(configs)
+    return loader
---
+++ data/datasets/ffhq.py
@@ -0,0 +1,11 @@
+
+from data.datasets.build import DATASETS_REGISTRY
+from data.utils import get_all_items
+
+
+@DATASETS_REGISTRY.register()
+def FFHQ(configs: dict):
+    data_dir = configs["DATA"]["DATASET"]["ROOT_DIR"]
+    img_extension = configs["DATA"]["DATASET"]["IMG_EXT"]
+    data_file_list = get_all_items(data_dir, img_extension)
+    return data_file_list
