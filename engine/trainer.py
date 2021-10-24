acc246bc8455a77ec1259dc12966f8eed412bd27
sunn-u <tjsntjsn20@gmail.com>
2021-10-24 23:42:58 +0900

    [2021-10-24] trainer 형태만 구성

---
+++ engine/trainer.py
@@ -0,0 +1,24 @@
+
+class Trainer:
+    def __init__(self, configs, logger, data_loader, generator, discriminator):
+        self.configs = configs
+        self.logger = logger
+        self.data_loader = data_loader
+        self.generator = generator
+        self.discriminator = discriminator
+
+        self.init_for_generator(configs)
+        self.init_for_discriminator(configs)
+
+    def init_for_generator(self, configs):
+        pass
+        # self.generator_criterion
+        # self.generator_optimizer
+
+    def init_for_discriminator(self, configs):
+        pass
+        # self.discriminator_criterion
+        # self.discriminator_optimizer
+
+    def train(self):
+        pass
