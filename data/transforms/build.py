d123f987dafb1c1191b7f4bae3cc949e0082cccc
sunn-u <tjsntjsn20@gmail.com>
2021-10-24 21:08:45 +0900

    [2021-10-24] add transforms func

---
+++ data/transforms/build.py
@@ -0,0 +1,19 @@
+
+from data.transforms.transform import Resize, Crop, ToTensor
+
+
+class Compose:
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, image):
+        for t in self.transforms:
+            image = t(image)
+        return image
+
+def build_transforms(configs: dict):
+    return Compose([
+        Resize(configs["TRANSFORMS"]["RESIZE"]["SIZE"], configs["TRANSFORMS"]["RESIZE"]["MODE"]),
+        Crop(configs["CROP"]),
+        ToTensor()
+    ])
--- data/transforms/transform.py
+++ data/transforms/transform.py
@@ -1,7 +1,23 @@
-from fvcore.transforms.transform import (
-    CropTransform,
-    HFlipTransform,
-    NoOpTransform,
-    Transform,
-    TransformList,
-)
+
+import torchvision.transforms.functional as F
+from torchvision.transforms.functional import _interpolation_modes_from_int
+
+
+class Crop:
+    def __init__(self, size: int):
+        self.size = size
+
+    def __call__(self, image):
+        return F.center_crop(image, self.size)
+
+class Resize:
+    def __init__(self, size: int, mode: int):
+        self.size = size
+        self.mode = _interpolation_modes_from_int(mode)
+
+    def __call__(self, image):
+        return F.reisze(image, (self.size, self.size), self.mode)
+
+class ToTensor:
+    def __call__(self, image):
+        return F.to_tensor(image)
